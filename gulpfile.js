cc2fe9262735ac9ea67d1b57e62e93934691994e
Ben Stapleton <webdev.ben@gmail.com>
2020-07-22 11:51:33 +0100

    Initial import of scss structure

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const strip = require('gulp-strip-css-comments');
+const clean = require('gulp-clean-css');
+
+// Paths
+let scssPath = "scss/";
+let cssPath = "css/";
+
+gulp.task("default", function(done) {
+    console.log("Generate CSS files " + (new Date()).toString());
+    gulp.src(scssPath + 'vendor/bootstrap/bootstrap-grid.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(cssPath));
+    gulp.src(scssPath + 'themes/dark.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(strip())
+        .pipe(gulp.dest(cssPath + "themes/"))
+        .pipe(clean({debug: true}, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize} default`);
+            console.log(`${details.name}: ${details.stats.minifiedSize} minified`);
+        }))
+        .pipe(gulp.dest(cssPath + 'dist/themes/'));
+    gulp.src(scssPath + 'themes/light.scss')
+        .pipe(sass().on("error", sass.logError))
+        .pipe(strip())
+        .pipe(gulp.dest(cssPath + 'themes/'))
+        .pipe(clean({debug: true}, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize} default`);
+            console.log(`${details.name}: ${details.stats.minifiedSize} minified`);
+        }))
+        .pipe(gulp.dest(cssPath + 'dist/themes/'));
+    done();
+});
